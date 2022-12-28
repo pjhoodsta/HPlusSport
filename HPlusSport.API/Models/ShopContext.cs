5381969bf375886226ea2728ec9df1bf28a7819f
Seung OK <pjhoodsta@hotmail.com>
2022-12-28 19:46:24 +0800

    2-4 EntityFrameworkCore with DbContext set up

---
+++ HPlusSport.API/Models/ShopContext.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace HPlusSport.API.Models
+{
+    public class ShopContext : DbContext
+    {
+        public ShopContext(DbContextOptions<ShopContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>()
+                .HasMany(c => c.Products)
+                .WithOne(a => a.Category)
+                .HasForeignKey(a => a.CategoryId);
+
+            modelBuilder.Seed();
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
